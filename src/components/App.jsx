d1b3ceb45214516fe9f22717e79cc0502d350abc
Ramon1108 <122237742+Ramon1108@users.noreply.github.com>
2023-08-01 22:41:09 +0300

    error fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,95 +7,83 @@ import SearchBar from './SearchBar';
 import Notiflix from 'notiflix';
 import Loader from './Loader';
 
-let page = 1;
-
 class App extends Component {
   state = {
     inputData: '',
     items: [],
 
+    page: 1,
     status: 'idle',
     totalHits: 0,
   };
-
-  handleSubmit = async inputData => {
-    page = 1;
-    if (inputData.trim() === '') {
-      Notiflix.Notify.info('You cannot search by empty field, try again.');
-      return;
-    } else {
-      try {
-        this.setState({ status: 'pending' });
-        const { totalHits, hits } = await fetchImgs(inputData, page);
-        if (hits.length < 1) {
-          this.setState({ status: 'idle' });
-          Notiflix.Notify.failure(
-            'Sorry, there are no images matching your search query. Please try again.'
-          );
-        } else {
-          this.setState({
-            items: hits,
-            inputData,
-            totalHits: totalHits,
-            status: 'resolved',
-          });
-        }
-      } catch (error) {
-        this.setState({ status: 'rejected' });
-      }
+  componentDidUpdate(_, prevState) {
+    if (
+      prevState.inputData !== this.state.inputData ||
+      prevState.page !== this.state.page
+    ) {
+      this.fetchImages();
     }
-  };
-  onNextPage = async () => {
-    this.setState({ status: 'pending' });
+  }
 
+  fetchImages = async () => {
     try {
-      const { hits } = await fetchImgs(this.state.inputData, (page += 1));
-      this.setState(prevState => ({
-        items: [...prevState.items, ...hits],
-        status: 'resolved',
-      }));
+      this.setState({ status: 'pending' });
+      const { totalHits, hits } = await fetchImgs(
+        this.state.inputData,
+        this.state.page
+      );
+      if (hits.length < 1) {
+        this.setState({ status: 'idle' });
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        this.setState(prevState => ({
+          items: prevState.page === 1 ? hits : [...prevState.items, ...hits],
+          totalHits: totalHits,
+          status: 'resolved',
+        }));
+      }
     } catch (error) {
       this.setState({ status: 'rejected' });
     }
   };
+
+  handleSubmit = async inputData => {
+    if (inputData.trim() === '') {
+      Notiflix.Notify.info('You cannot search by an empty field, try again.');
+    } else {
+      this.setState({ inputData, page: 1 });
+    }
+  };
+
+  onNextPage = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
   render() {
-    const { totalHits, status, items } = this.state;
-    if (status === 'idle') {
-      return (
-        <div className="App">
-          <SearchBar onSubmit={this.handleSubmit} />
-        </div>
-      );
-    }
-    if (status === 'pending') {
-      return (
-        <div className="App">
-          <SearchBar onSubmit={this.handleSubmit} />
-          <ImgGallery page={page} items={this.state.items} />
-          <Loader />
-          {totalHits > 12 && <Btn onClick={this.onNextPage} />}
-        </div>
-      );
-    }
-    if (status === 'rejected') {
-      return (
-        <div className="App">
-          <SearchBar onSubmit={this.handleSubmit} />
-          <p>Something wrong, try later</p>
-        </div>
-      );
-    }
-    if (status === 'resolved') {
-      return (
-        <div className="App">
-          <SearchBar onSubmit={this.handleSubmit} />
-          <ImgGallery page={page} items={this.state.items} />
-          {totalHits > 12 && totalHits > items.length && (
-            <Btn onClick={this.onNextPage} />
-          )}
-        </div>
-      );
-    }
+    const { totalHits, status, items, page } = this.state;
+
+    return (
+      <div className="App">
+        <SearchBar onSubmit={this.handleSubmit} />
+        {status === 'pending' && (
+          <>
+            <Loader />
+          </>
+        )}
+        {status === 'rejected' && <p>Something went wrong, try again later.</p>}
+        {status === 'resolved' && (
+          <>
+            {items.length > 0 && (
+              <ImgGallery page={page} items={this.state.items} />
+            )}
+            {totalHits > 12 && totalHits > items.length && (
+              <Btn onClick={this.onNextPage} />
+            )}
+          </>
+        )}
+      </div>
+    );
   }
 }
 export default App;
